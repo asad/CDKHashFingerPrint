86c6fd8fd3f227b651f138d8d407fb9bb71fab36
Syed Asad Rahman <s9asad@gmail.com>
2012-04-22 00:58:24 +0100

    refactoring the packages

--- src/benchmark/BenchmarkHashedFingerprint.java
+++ src/benchmark/BenchmarkHashedFingerprint.java
@@ -23,31 +23,32 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-package cdkhashedfingerprint;
+package benchmark;
 
-import cdkhashedfingerprint.helper.Data;
+import benchmark.helper.Base;
+import benchmark.helper.Data;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.util.BitSet;
 import java.util.HashMap;
 import java.util.Map;
 import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.fingerprint.FingerprinterTool;
+import org.openscience.cdk.fingerprint.IFingerprinter;
 import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.isomorphism.UniversalIsomorphismTester;
 import org.openscience.smsd.algorithm.vflib.substructure.VF2;
 
-/** 
- *  Test new FP 
- *java -jar dist/CDKHashedFingerprint.jar test/data/mol new  2  1000
- * Test CDK default FP
- *java -jar dist/CDKHashedFingerprint.jar test/data/mol cdk  2  1000
- * Test new FP with ring matcher 
- *java -jar dist/CDKHashedFingerprint.jar test/data/mol new  1  1000
+/**
+ * Test new FP java -jar dist/CDKHashedFingerprint.jar test/data/mol new 2 1000 Test CDK default FP java -jar
+ * dist/CDKHashedFingerprint.jar test/data/mol cdk 2 1000 Test new FP with ring matcher java -jar
+ * dist/CDKHashedFingerprint.jar test/data/mol new 1 1000
+ *
  * @author Syed Asad Rahman <asad@ebi.ac.uk>
  */
-public class BenchmarkIsomorphism extends Base {
+public class BenchmarkHashedFingerprint extends Base {
 
     private static long TP;
     private static long FP;
@@ -55,19 +56,22 @@ public class BenchmarkIsomorphism extends Base {
     private static long TN;
     private static long HITS;
     private static Map<String, Data> dataMap = new HashMap<String, Data>();
+    private static IFingerprinter cdkFingerprint = new org.openscience.cdk.fingerprint.Fingerprinter(1024);
+    private static fingerprints.interfaces.IFingerprinter fingerprint1 = new fingerprints.HashedFingerprinter(1024);
+    private static fingerprints.interfaces.IFingerprinter fingerprint2 = new fingerprints.HashedBloomFingerprinter(1024);
 
     /**
      * @param args the command line arguments
      * @throws FileNotFoundException
      * @throws CDKException
-     * @throws IOException  
+     * @throws IOException
      */
     public static void main(String[] args) throws FileNotFoundException, CDKException, IOException {
         String s = System.getProperty("user.home") + File.separator + args[0];
         File directory = new File(s);
         int expectedDataSize = 100;
-        if (args.length >= 2) {
-            expectedDataSize = Integer.valueOf(args[1]);
+        if (args.length >= 4) {
+            expectedDataSize = Integer.valueOf(args[3]);
         }
         System.out.print("\n***************************************\n");
 
@@ -84,13 +88,21 @@ public class BenchmarkIsomorphism extends Base {
         System.out.print("TN:" + "\t");
         System.out.print("FN:" + "\t");
         System.out.print("ACCURACY:" + "\t");
-        /*TRUE POSITIVE RATE*/
+        /*
+         * TRUE POSITIVE RATE
+         */
         System.out.print("TPR:" + "\t");
-        /*FALSE POSITIVE RATE*/
+        /*
+         * FALSE POSITIVE RATE
+         */
         System.out.print("FPR:" + "\t");
         System.out.println("Time (mins): ");
         System.out.print("------------------------------------------------------------------------------\n");
 
+        if (args.length > 2 && args[2].equals("1")) {
+            fingerprint1.setRespectRingMatches(true);
+            fingerprint2.setRespectRingMatches(true);
+        }
 
         for (int k = 0; k < molecules.size(); k += interval) {
             int counter = 1;
@@ -100,10 +112,20 @@ public class BenchmarkIsomorphism extends Base {
                     continue;
                 }
                 try {
-                    dataMap.put(inchiKey, new Data(ac));
+                    BitSet hashedFingerPrint = null;
+                    if (args.length > 1 && args[1].equals("cdk")) {
+                        hashedFingerPrint = getCDKFingerprint(ac);
+                        dataMap.put(inchiKey, new Data(hashedFingerPrint, ac));
+                    } else if (args.length > 1 && args[1].equals("hash")) {
+                        hashedFingerPrint = getHashedFingerprint(ac);
+                        dataMap.put(inchiKey, new Data(hashedFingerPrint, ac));
+                    } else if (args.length > 1 && args[1].equals("hashbloom")) {
+                        hashedFingerPrint = getHashedBloomFingerprint(ac);
+                        dataMap.put(inchiKey, new Data(hashedFingerPrint, ac));
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
-                    System.err.println("error in generating graph: " + ac.getID());
+                    System.err.println("error in generating fp: " + ac.getID());
                 }
 
                 if (counter++ == interval) {
@@ -120,22 +142,19 @@ public class BenchmarkIsomorphism extends Base {
             long startTime = System.currentTimeMillis();
             for (Data fragment : dataMap.values()) {
                 for (Data original : dataMap.values()) {
-                    boolean UIT = false;
-                    boolean TrueMatch = false;
-                    UIT = UniversalIsomorphismTester.isSubgraph(
-                            original.getAtomContainer(),
-                            fragment.getAtomContainer());
-                    TrueMatch = new VF2(fragment.getAtomContainer(), original.getAtomContainer(), true, false).isSubgraph();
+                    boolean FPMatch = FingerprinterTool.isSubset(
+                            original.getFingerprint(),
+                            fragment.getFingerprint());
+                    VF2 sub = sub = new VF2(fragment.getAtomContainer(), original.getAtomContainer(), true, false);
+                    boolean TrueMatch = sub.isSubgraph();
 
-                    if (UIT && TrueMatch) {
+                    if (FPMatch && TrueMatch) {
                         TP++;
-                    } else if (UIT && !TrueMatch) {
-//                        System.out.println("\nQ " + fragment.getAtomContainer().getID());
-//                        System.out.println("T " + original.getAtomContainer().getID());
+                    } else if (FPMatch && !TrueMatch) {
                         FP++;
-                    } else if (!UIT && TrueMatch) {
+                    } else if (!FPMatch && TrueMatch) {
                         FN++;
-                    } else if (!UIT && !TrueMatch) {
+                    } else if (!FPMatch && !TrueMatch) {
                         TN++;
                     }
                     HITS++;
@@ -170,5 +189,17 @@ public class BenchmarkIsomorphism extends Base {
     private static String getElapsedTime(long startTime) {
         DecimalFormat df = new DecimalFormat("#.##");
         return df.format((System.currentTimeMillis() - startTime) / (1000 * 60.0)); //*60 for hrs
+    }
+
+    private static BitSet getCDKFingerprint(IAtomContainer ac) throws CDKException {
+        return cdkFingerprint.getFingerprint(ac);
+    }
+
+    private static BitSet getHashedFingerprint(IAtomContainer ac) throws CDKException {
+        return fingerprint1.getFingerprint(ac);
+    }
+
+    private static BitSet getHashedBloomFingerprint(IAtomContainer ac) throws CDKException {
+        return fingerprint2.getFingerprint(ac);
     }
 }
--- src/benchmark/BenchmarkIsomorphism.java
+++ src/benchmark/BenchmarkIsomorphism.java
@@ -23,33 +23,30 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-package cdkhashedfingerprint;
+package benchmark;
 
-import cdkhashedfingerprint.helper.Data;
+import benchmark.helper.Base;
+import benchmark.helper.Data;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
-import java.util.BitSet;
 import java.util.HashMap;
 import java.util.Map;
 import org.openscience.cdk.exception.CDKException;
-import org.openscience.cdk.fingerprint.FingerprinterTool;
-import org.openscience.cdk.fingerprint.IFingerprinter;
 import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.isomorphism.UniversalIsomorphismTester;
 import org.openscience.smsd.algorithm.vflib.substructure.VF2;
 
-/** 
- *  Test new FP 
- *java -jar dist/CDKHashedFingerprint.jar test/data/mol new  2  1000
- * Test CDK default FP
- *java -jar dist/CDKHashedFingerprint.jar test/data/mol cdk  2  1000
- * Test new FP with ring matcher 
- *java -jar dist/CDKHashedFingerprint.jar test/data/mol new  1  1000
+/**
+ * Test new FP java -jar dist/CDKHashedFingerprint.jar test/data/mol new 2 1000 Test CDK default FP java -jar
+ * dist/CDKHashedFingerprint.jar test/data/mol cdk 2 1000 Test new FP with ring matcher java -jar
+ * dist/CDKHashedFingerprint.jar test/data/mol new 1 1000
+ *
  * @author Syed Asad Rahman <asad@ebi.ac.uk>
  */
-public class BenchmarkHashedFingerprint extends Base {
+public class BenchmarkIsomorphism extends Base {
 
     private static long TP;
     private static long FP;
@@ -57,22 +54,19 @@ public class BenchmarkHashedFingerprint extends Base {
     private static long TN;
     private static long HITS;
     private static Map<String, Data> dataMap = new HashMap<String, Data>();
-    private static IFingerprinter cdkFingerprint = new org.openscience.cdk.fingerprint.Fingerprinter(1024);
-    private static fingerprints.interfaces.IFingerprinter fingerprint1 = new fingerprints.HashedFingerprinter(1024);
-    private static fingerprints.interfaces.IFingerprinter fingerprint2 = new fingerprints.HashedBloomFingerprinter(1024);
 
     /**
      * @param args the command line arguments
      * @throws FileNotFoundException
      * @throws CDKException
-     * @throws IOException  
+     * @throws IOException
      */
     public static void main(String[] args) throws FileNotFoundException, CDKException, IOException {
         String s = System.getProperty("user.home") + File.separator + args[0];
         File directory = new File(s);
         int expectedDataSize = 100;
-        if (args.length >= 4) {
-            expectedDataSize = Integer.valueOf(args[3]);
+        if (args.length >= 2) {
+            expectedDataSize = Integer.valueOf(args[1]);
         }
         System.out.print("\n***************************************\n");
 
@@ -89,18 +83,18 @@ public class BenchmarkHashedFingerprint extends Base {
         System.out.print("TN:" + "\t");
         System.out.print("FN:" + "\t");
         System.out.print("ACCURACY:" + "\t");
-        /*TRUE POSITIVE RATE*/
+        /*
+         * TRUE POSITIVE RATE
+         */
         System.out.print("TPR:" + "\t");
-        /*FALSE POSITIVE RATE*/
+        /*
+         * FALSE POSITIVE RATE
+         */
         System.out.print("FPR:" + "\t");
         System.out.println("Time (mins): ");
         System.out.print("------------------------------------------------------------------------------\n");
 
-        if (args.length > 2 && args[2].equals("1")) {
-            fingerprint1.setRespectRingMatches(true);
-            fingerprint2.setRespectRingMatches(true);
-        }
-        
+
         for (int k = 0; k < molecules.size(); k += interval) {
             int counter = 1;
             for (String inchiKey : molecules.keySet()) {
@@ -109,20 +103,10 @@ public class BenchmarkHashedFingerprint extends Base {
                     continue;
                 }
                 try {
-                    BitSet hashedFingerPrint = null;
-                    if (args.length > 1 && args[1].equals("cdk")) {
-                        hashedFingerPrint = getCDKFingerprint(ac);
-                        dataMap.put(inchiKey, new Data(hashedFingerPrint, ac));
-                    } else if (args.length > 1 && args[1].equals("hash")) {
-                        hashedFingerPrint = getHashedFingerprint(ac);
-                        dataMap.put(inchiKey, new Data(hashedFingerPrint, ac));
-                    } else if (args.length > 1 && args[1].equals("hashbloom")) {
-                        hashedFingerPrint = getHashedBloomFingerprint(ac);
-                        dataMap.put(inchiKey, new Data(hashedFingerPrint, ac));
-                    }
+                    dataMap.put(inchiKey, new Data(ac));
                 } catch (Exception e) {
                     e.printStackTrace();
-                    System.err.println("error in generating fp: " + ac.getID());
+                    System.err.println("error in generating graph: " + ac.getID());
                 }
 
                 if (counter++ == interval) {
@@ -139,19 +123,22 @@ public class BenchmarkHashedFingerprint extends Base {
             long startTime = System.currentTimeMillis();
             for (Data fragment : dataMap.values()) {
                 for (Data original : dataMap.values()) {
-                    boolean FPMatch = FingerprinterTool.isSubset(
-                            original.getFingerprint(),
-                            fragment.getFingerprint());
-                    VF2 sub = sub = new VF2(fragment.getAtomContainer(), original.getAtomContainer(), true, false);
-                    boolean TrueMatch = sub.isSubgraph();
+                    boolean UIT = false;
+                    boolean TrueMatch = false;
+                    UIT = UniversalIsomorphismTester.isSubgraph(
+                            original.getAtomContainer(),
+                            fragment.getAtomContainer());
+                    TrueMatch = new VF2(fragment.getAtomContainer(), original.getAtomContainer(), true, false).isSubgraph();
 
-                    if (FPMatch && TrueMatch) {
+                    if (UIT && TrueMatch) {
                         TP++;
-                    } else if (FPMatch && !TrueMatch) {
+                    } else if (UIT && !TrueMatch) {
+//                        System.out.println("\nQ " + fragment.getAtomContainer().getID());
+//                        System.out.println("T " + original.getAtomContainer().getID());
                         FP++;
-                    } else if (!FPMatch && TrueMatch) {
+                    } else if (!UIT && TrueMatch) {
                         FN++;
-                    } else if (!FPMatch && !TrueMatch) {
+                    } else if (!UIT && !TrueMatch) {
                         TN++;
                     }
                     HITS++;
@@ -186,17 +173,5 @@ public class BenchmarkHashedFingerprint extends Base {
     private static String getElapsedTime(long startTime) {
         DecimalFormat df = new DecimalFormat("#.##");
         return df.format((System.currentTimeMillis() - startTime) / (1000 * 60.0)); //*60 for hrs
-    }
-
-    private static BitSet getCDKFingerprint(IAtomContainer ac) throws CDKException {
-        return cdkFingerprint.getFingerprint(ac);
-    }
-
-    private static BitSet getHashedFingerprint(IAtomContainer ac) throws CDKException {
-        return fingerprint1.getFingerprint(ac);
-    }
-
-    private static BitSet getHashedBloomFingerprint(IAtomContainer ac) throws CDKException {
-        return fingerprint2.getFingerprint(ac);
     }
 }
