a79e5215d22299316ce607c0fe5d3dc4c56a98e9
Asad Rahman <s9asad@gmail.com>
2021-02-16 03:12:10 +0000

    reduce fp cases by mcb rings

--- fingerprinter/src/main/java/com/bioinception/chem/fp/fingerprints/bi/ScaffoldHashedFingerprinter.java
+++ fingerprinter/src/main/java/com/bioinception/chem/fp/fingerprints/bi/ScaffoldHashedFingerprinter.java
@@ -190,9 +190,11 @@ public class ScaffoldHashedFingerprinter extends AbstractFingerprinter implement
         logger.debug("Finished Aromaticity Detection");
 
         // all cycles or relevant or essential
-        CycleFinder cf = Cycles.or(Cycles.all(),
-                Cycles.or(Cycles.relevant(),
-                        Cycles.essential()));
+//        CycleFinder cf = Cycles.or(Cycles.all(),
+//                Cycles.or(Cycles.relevant(),
+//                        Cycles.essential()));
+        //minimal cycle basis
+        CycleFinder cf = Cycles.mcb();
 
         IRingSet rings = null;
         try {
